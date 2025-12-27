2fce949cefd3363a6f4ca8a3b72ffe765590d796
Erwin Holzhauser <erwinholzhauser.eh@gmail.com>
2025-12-27 02:30:22 +0100

    sada

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import Contact from "../pages/ContactPage.vue";
 import AboutPage from "../pages/AboutPage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     { path: "/", name: "home", component: HomePage },
     { path: "/home", redirect: "/" },
