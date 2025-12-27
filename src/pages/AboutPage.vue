4a4322bb58394cab6a85be4225159afb81beba59
Erwin Holzhauser <erwinholzhauser.eh@gmail.com>
2025-12-27 02:44:59 +0100

    sada

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,14 +1,27 @@
 <template>
   <div> About</div>
   <HeroComponent />
-  <div
-    class="fussballde_widget"
-    data-id="6bdd5140-b8f6-4e0f-b8df-b098fee30e6e"
-    data-type="table"
-    style="width: 100%">
-  </div>
+  <section class="widgetShell">
+    <h2>Aktuelle Tabelle</h2>
+    <div
+      class="fussballde_widget"
+      data-id="6bdd5140-b8f6-4e0f-b8df-b098fee30e6e"
+      data-type="table"
+      style="width: 100%">
+    </div>
+  </section>
 </template>
 
 <script setup>
 import HeroComponent from "../components/HeroComponent.vue";
 </script>
+
+<style scoped>
+.widgetShell {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 16px;
+  border-radius: 12px;
+  border: 1px solid #ddd;
+}
+</style>
